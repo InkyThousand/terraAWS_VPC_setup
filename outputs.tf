a6613c7dbee390e86812825774c8a8fd8d7dbb56
Pavel Losiev <inky.thousand@gmail.com>
2025-07-22 14:12:06 +0200

    Rewrite outputs instuction

--- outputs.tf
+++ outputs.tf
@@ -37,15 +37,11 @@ output "connection_instructions" {
     
     === CONNECTION INSTRUCTIONS ===
     
-    1. SSH to Bastion host:
-       ssh -i ${var.bastion_key} ec2-user@${aws_instance.bastion.public_ip}
-    
-    2. From Bastion, SSH to WordPress:
-       First, copy your SSH key to the Bastion:
-       scp -i ${var.bastion_key} ${var.bastion_key} ec2-user@${aws_instance.bastion.public_ip}:~/
-       
-       Then SSH to the WordPress server:
-       ssh -i ~/ssh-key-${var.environment}.pem ec2-user@<WORDPRESS_INSTANCE_IP>
+      # From your local machine with agent forwarding enabled
+      ssh -A -i ~/.ssh/bastion_key ec2-user@${aws_instance.bastion.public_ip}
+
+      # Then from the bastion host
+      ssh ec2-user@<wordpress-private-ip>
     
     🌐 WORDPRESS ACCESS (Secure):
        URL: http://${aws_lb.wordpress_alb.dns_name}
